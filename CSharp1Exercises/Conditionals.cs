b80dcba6a3e517c6b3898b14eecf41a84c0d3cc3
Alisher Sadikov <alishersadikov@gmail.com>
2020-04-29 07:14:34 -0500

    * ImageDirection.cs:
    * Conditionals.cs: Image direction based on height/width

--- CSharp1Exercises/Conditionals.cs
+++ CSharp1Exercises/Conditionals.cs
@@ -1,5 +1,5 @@
 ﻿using System;namespace CSharp1Exercises.ControlFlow{    public class Conditionals    {        static void Main(string[] args)
         {
-            new ValidNumber();
+            new ImageDirection();
         }
     }}
---
+++ CSharp1Exercises/ImageDirection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CSharp1Exercises
+{
+    public class ImageDirection
+    {
+        public ImageDirection()
+        {
+            Console.Write("Enter image width: ");
+            var width_input = Console.ReadLine();
+            var width = Convert.ToInt32(width_input);
+
+            Console.Write("Enter image height: ");
+            var height_input = Console.ReadLine();
+            var height = Convert.ToInt32(height_input);
+
+            var style = width > height ? "landscape" : "portrait";
+
+            Console.WriteLine("This image is {0}", style);
+        }
+    }
+}
