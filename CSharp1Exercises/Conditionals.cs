56c8bdc02d304e01a7360436ee45b837aa9db851
Alisher Sadikov <alishersadikov@gmail.com>
2020-04-29 06:54:42 -0500

    Extract logic into a class

---
+++ CSharp1Exercises/Conditionals.cs
@@ -0,0 +1,5 @@
+﻿using System;namespace CSharp1Exercises.ControlFlow{    public class Conditionals    {        static void Main(string[] args)
+        {
+            new MaxFinder();
+        }
+    }}
---
+++ CSharp1Exercises/MaxFinder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp1Exercises.ControlFlow
+{
+    internal class MaxFinder
+    {
+        public MaxFinder()
+        {
+            Console.Write("Enter the first number: ");            var input_1 = Console.ReadLine();            var number_1 = Convert.ToInt32(input_1);            Console.Write("Enter the second number: ");            var input_2 = Console.ReadLine();            var number_2 = Convert.ToInt32(input_2);            var max = Math.Max(number_1, number_2);            Console.WriteLine("The bigger integer is: {0}", max);
+        }
+    }
+}
