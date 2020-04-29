c0f7a416d740ac8fc4098b765f0a6fe28ffd7c29
Alisher Sadikov <alishersadikov@gmail.com>
2020-04-29 07:27:16 -0500

    Speed checker solution

---
+++ CSharp1Exercises/SpeedChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace CSharp1Exercises
+{
+    public class SpeedChecker
+    {
+        public SpeedChecker()
+        {
+            Console.WriteLine("Enter the speed limit: ");
+            var speed_limit = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the speed of the car: ");
+            var actual_speed = Convert.ToInt32(Console.ReadLine());
+
+            if (actual_speed <= speed_limit)
+            {
+                Console.WriteLine("You are OK.");
+                return;
+            }
+
+            var points = (actual_speed - speed_limit) / 5;
+
+            if (points <= 12)
+            {
+                Console.WriteLine("Points: {0}", points);
+                return;
+            }
+
+            Console.WriteLine("License suspended");
+        }
+    }
+}
