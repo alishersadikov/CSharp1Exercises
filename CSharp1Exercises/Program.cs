adf04c64097b7416cd17ef7a62427081658746ca
Alisher Sadikov <alishersadikov@gmail.com>
2020-04-30 19:15:15 -0500

    Solve pascal case converter

--- CSharp1Exercises/Program.cs
+++ CSharp1Exercises/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         { 
-            new Strings.TimeValidator();
+            new Strings.CaseConverter();
         }
     }
 }
---
+++ CSharp1Exercises/Strings/CaseConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp1Exercises.Strings
+{
+    internal class CaseConverter
+    {
+        // Write a program and ask the user to enter a few words separated by
+        // a space.Use the words to create a variable name with PascalCase.
+        // For example, if the user types: "number of students", display
+        // "NumberOfStudents". Make sure that the program is not dependent on
+        // the input.So, if the user types "NUMBER OF STUDENTS", the program
+        // should still display "NumberOfStudents".
+
+        public CaseConverter()
+        {
+            Console.WriteLine("Enter a few world separated by space:");
+            var input = Console.ReadLine();
+
+            var words = input.Split(' ');
+
+            var pascalCaseWords = "";
+
+            foreach (var word in words)
+            {
+                var converted = Char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                pascalCaseWords += converted;
+            }
+
+            Console.WriteLine(pascalCaseWords);
+        }
+    }
+}
